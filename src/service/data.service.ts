fc63227a056db52d212645d3f4777b5d3e9aa77f
sathak <sathakathullas@ilink-systems.com>
2019-09-07 00:01:41 +0530

    2 files modified

--- src/service/data.service.ts
+++ src/service/data.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';  // Import it up here
 export class DataService {
 
   constructor(private http: HttpClient) { }
-private _jsonURL = "../data/data.json";
+private _jsonURL = "https://raw.githubusercontent.com/sathak/angular-quiz-app/master/src/data/data.json";
   getData() {
     return this.http.get(this._jsonURL, {responseType : 'json'})
   }
